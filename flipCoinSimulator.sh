17f2d536ee12a767db5db1a5f5e7c97431e2c069
Ranbir7925 <35680405+Ranbir7925@users.noreply.github.com>
2020-07-12 20:53:22 +0530

    cheaking the winner

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,8 +1,34 @@
 #!/bin/bash
+headCount=0
+tailCount=0
+count=0
+while [ $count -ne 21 ]
+do
+	coin=$((RANDOM%2))
+	if [ $coin -eq 0 ]
+	then
+		((headCount++))
+	else
+		((tailCount++))
+	fi
+	((count++))
 
-if [ $((RANDOM%2)) -eq 1 ]
-then
-	echo "Heads.."
-else
-	echo "Tails.."
-fi
+done
+
+function checkWinner() {
+	headCount=$1
+   tailCount=$2
+	if [[ $2 -gt $1 ]]
+	then
+		echo "Number of Heads is: "$headCount
+   	echo "Number of Tails is: "$tailCount
+		echo "Tails won by" $((tailCount-headCount))
+	else
+		echo "Number of Heads is: "$headCount
+     	echo "Number of Tails is: "$tailCount
+		echo "Heads won by" $((headCount-tailCount))
+	fi
+}
+
+checkWinner $headCount $tailCount
+
