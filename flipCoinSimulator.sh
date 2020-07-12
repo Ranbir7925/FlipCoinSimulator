90dca56d19d6599ea6f1afcaae92973daf7e1a88
Ranbir7925 <35680405+Ranbir7925@users.noreply.github.com>
2020-07-12 21:13:05 +0530

    Showed winner/losser/tie and its count

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -2,7 +2,7 @@
 headCount=0
 tailCount=0
 count=0
-while [ $count -ne 21 ]
+while [ $count ]
 do
 	coin=$((RANDOM%2))
 	if [ $coin -eq 0 ]
@@ -13,16 +13,27 @@ do
 	fi
 	((count++))
 
+	if [[ (headCount -eq 21) || (tailCount -eq 21) ]]
+	then
+		break
+	else
+		continue
+	fi
 done
 
 function checkWinner() {
 	headCount=$1
    tailCount=$2
-	if [[ $2 -gt $1 ]]
+	if [[ $tailCount -gt $headCount ]]
 	then
 		echo "Number of Heads is: "$headCount
    	echo "Number of Tails is: "$tailCount
 		echo "Tails won by" $((tailCount-headCount))
+	elif [[ $tailCount -eq $headCount ]]
+	then
+		echo "Number of Heads is: "$headCount
+      echo "Number of Tails is: "$tailCount
+		echo "Its tie Lets try one more time"
 	else
 		echo "Number of Heads is: "$headCount
      	echo "Number of Tails is: "$tailCount
@@ -31,4 +42,3 @@ function checkWinner() {
 }
 
 checkWinner $headCount $tailCount
-
